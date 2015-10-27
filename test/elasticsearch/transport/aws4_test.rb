b47501b0754a3db3ce586e031b89eb72e97948a8
Filip Tepper <filip@tepper.pl>
2015-10-27 10:26:08 +0100

    verify only headers required for AWS authentication

--- test/elasticsearch/transport/aws4_test.rb
+++ test/elasticsearch/transport/aws4_test.rb
@@ -14,22 +14,6 @@ describe Elasticsearch::Transport::AWS4 do
         region: "us-east-1"
       }
     )
-
-    @request = stub_request(
-      :get,
-      "https://search-xyz-abcdefghijklmmnopqrtuvwxyz.us-east-1.es.amazonaws.com/_search"
-    ).with(
-      headers: {
-        "Accept"               => "*/*",
-        "Accept-Encoding"      => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "Authorization"        => /.+/,
-        "Content-Type"         => "application/x-www-form-urlencoded",
-        "Host"                 => "search-XYZ-ABCDEFGHIJKLMMNOPQRTUVWXYZ.us-east-1.es.amazonaws.com",
-        "User-Agent"           => "Faraday v0.9.2",
-        "X-Amz-Content-Sha256" => /.+/,
-        "X-Amz-Date"           => "20150101T110000Z"
-      }
-    ).to_return(status: 200, body: "", headers: {})
   end
 
   after do
@@ -38,6 +22,14 @@ describe Elasticsearch::Transport::AWS4 do
 
   it "adds Authorization header" do
     @client.search
-    assert_requested @request
+    assert_requested(
+      :get,
+      "https://search-xyz-abcdefghijklmmnopqrtuvwxyz.us-east-1.es.amazonaws.com/_search",
+      headers: {
+        "Authorization"        => /.+/,
+        "X-Amz-Content-Sha256" => /.+/,
+        "X-Amz-Date"           => "20150101T110000Z"
+      }
+    )
   end
 end
