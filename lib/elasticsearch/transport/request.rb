609ac6928369fe9c1126959311816198f01f887c
Filip Tepper <filip@tepper.pl>
2015-10-27 13:04:13 +0100

    add query parameters to `endpoint` when signing request

--- lib/elasticsearch/transport/request.rb
+++ lib/elasticsearch/transport/request.rb
@@ -3,7 +3,11 @@ require "faraday"
 module Faraday
   class Request
     def endpoint
-      URI.parse(self.path)
+      URI.parse(
+        self.params.any? ?
+          "#{self.path}?#{params.map { |key, value| "#{key}=#{value}" }.join("&")}" :
+          self.path
+      )
     end
 
     def http_method
