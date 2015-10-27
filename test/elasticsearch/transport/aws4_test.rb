cef05a52379ce087782498101f0953d02b6df733
Filip Tepper <filip@tepper.pl>
2015-10-27 09:30:46 +0100

    more generic header matching with regular expression

--- test/elasticsearch/transport/aws4_test.rb
+++ test/elasticsearch/transport/aws4_test.rb
@@ -22,7 +22,7 @@ describe Elasticsearch::Transport::AWS4 do
       headers: {
         "Accept"               => "*/*",
         "Accept-Encoding"      => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "Authorization"        => "AWS4-HMAC-SHA256 Credential=KEY_KEY_KEY_KEY/20150101/us-east-1/es/aws4_request, SignedHeaders=host;user-agent;x-amz-content-sha256;x-amz-date, Signature=a613a4b804bbe279e338b5d5fdd5caa9cb87efe61e7f54920eca7885f6ca822d",
+        "Authorization"        => /AWS4-HMAC-SHA256 Credential=KEY_KEY_KEY_KEY\/20150101\/us-east-1\/es\/aws4_request, SignedHeaders=host;user-agent;x-amz-content-sha256;x-amz-date, Signature=.*/,
         "Content-Type"         => "application/x-www-form-urlencoded",
         "Host"                 => "search-XYZ-ABCDEFGHIJKLMMNOPQRTUVWXYZ.us-east-1.es.amazonaws.com",
         "User-Agent"           => "Faraday v0.9.2",
