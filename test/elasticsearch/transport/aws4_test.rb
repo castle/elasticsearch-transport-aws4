6b53d94a212ba83980450b7f0c016279cdd680cb
Filip Tepper <filip@tepper.pl>
2015-10-27 09:36:21 +0100

    regular expression matching for generated headers

--- test/elasticsearch/transport/aws4_test.rb
+++ test/elasticsearch/transport/aws4_test.rb
@@ -22,11 +22,11 @@ describe Elasticsearch::Transport::AWS4 do
       headers: {
         "Accept"               => "*/*",
         "Accept-Encoding"      => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "Authorization"        => /AWS4-HMAC-SHA256 Credential=KEY_KEY_KEY_KEY\/20150101\/us-east-1\/es\/aws4_request, SignedHeaders=host;user-agent;x-amz-content-sha256;x-amz-date, Signature=.*/,
+        "Authorization"        => /.+/,
         "Content-Type"         => "application/x-www-form-urlencoded",
         "Host"                 => "search-XYZ-ABCDEFGHIJKLMMNOPQRTUVWXYZ.us-east-1.es.amazonaws.com",
         "User-Agent"           => "Faraday v0.9.2",
-        "X-Amz-Content-Sha256" => "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+        "X-Amz-Content-Sha256" => /.+/,
         "X-Amz-Date"           => "20150101T110000Z"
       }
     ).to_return(status: 200, body: "", headers: {})
